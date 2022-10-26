dc6f2d06434fe0f5d9e1551e4c40da43b20a3326
ShreyasRaman7 <71574041+ShreyasRaman7@users.noreply.github.com>
2022-10-25 20:26:34 -0400

    Shreyas first commit-forked

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ import {
 /*
     This is our application's top-level component.
     
-    @author McKilla Gorilla
+    @author McKilla Gorilla.
 */
 /*
   This is the entry-point for our application. Notice that we
